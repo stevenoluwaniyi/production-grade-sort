c0b1b8b73aa75561e8282b8f8dfe87d4c181e766
Matt Momont <matt.momont@gmail.com>
2019-10-14 14:26:46 -0500

    Handling missing event parameters

--- handler.py
+++ handler.py
@@ -1,17 +1,26 @@
 import json
 
 
-def sort(event, context):
-    query = event['queryStringParameters']['numbers']
-    if query is not None:
-        numbers = query_string_to_list(query)
-    else:
-        numbers = [3, 2, 1]
+def sort(event):
+    numbers = get_numbers_or_default(event)
     numbers.sort()
     return create_response(numbers)
 
 
 # Helper functions
+def get_numbers_or_default(event):
+    query_string = event['queryStringParameters']
+    if query_string is not None:
+        numbers_string = query_string["numbers"]
+        if numbers_string is not None:
+            numbers = query_string_to_list(query_string)
+        else:
+            numbers = [3, 2, 1]
+    else:
+        numbers = [3, 2, 1]
+    return numbers
+
+
 def query_string_to_list(query_string):
     return list(map(int, query_string.split(',')))
 
